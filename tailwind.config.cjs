aacd6c4eee35f542ce12d8a9ebf8e2dee93af2ff
Dylan Buchi <devybuchi@gmail.com>
2022-11-12 14:36:23 -0300

    Add main colors to tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "icons-color": "#767676",
+        "logo-color": "#cc1100",
+        "home-bg-color": "#111111",
+        "profile-bg-color": "#e9e9e9",
+        "bg-color": "#fff",
+        "icons-hover-color": "#e9e9e9",
+      },
+    },
   },
   plugins: [],
 };
