5cee24a64a8837110b584f75e4d185fb65e2bcdb
Dylan Buchi <devybuchi@gmail.com>
2022-11-14 11:55:13 -0300

    Add PinList component

---
+++ src/components/Pin/PinList.tsx
@@ -0,0 +1,18 @@
+import { TPin } from "../../assets/images/pins/pins";
+import Pin from "./Pin";
+
+interface PinListProps {
+  pins: TPin[];
+}
+
+const PinList = ({ pins }: PinListProps): JSX.Element => {
+  return (
+    <ul className="pt-1 pb-4 px-10 sm:px-20 columns-2 sm:columns-3 md:columns-4 lg:columns-5 xl:columns-6 space-x-4 space-y-4">
+      {pins.map((item) => (
+        <Pin key={["pin-", item].join("")} url={item} />
+      ))}
+    </ul>
+  );
+};
+
+export default PinList;
