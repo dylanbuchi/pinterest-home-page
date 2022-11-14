1654ebfb4ad87ca8f3f7a9a16bcd5c204dee0744
Dylan Buchi <devybuchi@gmail.com>
2022-11-14 14:37:45 -0300

    Update NavbarIcon styles

--- src/components/navbar/NavbarIcon.tsx
+++ src/components/navbar/NavbarIcon.tsx
@@ -7,7 +7,7 @@ interface NavbarIconProps {
 const NavbarIcon = ({ icon, classes = "" }: NavbarIconProps): JSX.Element => {
   return (
     <div
-      className={`text-icons-color grid place-items-center w-[2.6rem] h-[2.6rem] sm:w-[4rem] sm:h-[4rem] hover:rounded-full hover:cursor-pointer hover:bg-icons-hover-color text-[1.5rem] ${classes}`}
+      className={`text-icons-color grid place-items-center w-[2.6rem] h-[2.6rem] sm:w-[3.5rem] sm:h-[3.5rem] hover:rounded-full hover:cursor-pointer hover:bg-icons-hover-color text-[1.5rem] ${classes}`}
     >
       {icon}
     </div>
