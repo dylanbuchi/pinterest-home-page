f7428050cdc1b75bb707895f1d567fb57a0b22a9
Dylan Buchi <devybuchi@gmail.com>
2022-11-14 14:38:34 -0300

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Navbar from "./components/Navbar/Navbar";
+import pins from "./assets/images/pins/pins";
+import PinList from "./components/Pin/PinList";
+
 const App = (): JSX.Element => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <PinList pins={pins} />
+    </>
+  );
 };
 
 export default App;
