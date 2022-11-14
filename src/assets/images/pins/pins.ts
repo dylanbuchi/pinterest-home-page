c4b40f525168709242c99b5cde0510cedf1e1f34
Dylan Buchi <devybuchi@gmail.com>
2022-11-14 14:19:25 -0300

    Add 3 more pins

--- src/assets/images/pins/pins.ts
+++ src/assets/images/pins/pins.ts
@@ -7,8 +7,24 @@ import pin6 from "./red-zeppelin-Apu5ltNZ6_c-unsplash.jpg";
 import pin7 from "./resource-database-AmnCaSI794w-unsplash.jpg";
 import pin8 from "./road-ahead-5bibIzmmdrA-unsplash.jpg";
 import pin9 from "./taisiia-shestopal-ub6GOtZjUZI-unsplash.jpg";
+import pin10 from "./raelle-gann-owens-uCeQ47oTS-4-unsplash.jpg";
+import pin11 from "./robert-bye-ePXvFiKAsOY-unsplash.jpg";
+import pin12 from "./josh-withers-S3gHPOLsp8k-unsplash.jpg";
 
 export type TPin = string;
-const pins: TPin[] = [pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, pin9];
+const pins: TPin[] = [
+  pin1,
+  pin2,
+  pin3,
+  pin4,
+  pin5,
+  pin6,
+  pin7,
+  pin8,
+  pin9,
+  pin10,
+  pin11,
+  pin12,
+];
 
 export default pins;
