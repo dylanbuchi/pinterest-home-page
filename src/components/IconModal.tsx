fb6ef0862758207ddf43a61929edf477cbc21a60
Dylan Buchi <devybuchi@gmail.com>
2022-11-15 08:45:48 -0300

    Add IconModal component

---
+++ src/components/IconModal.tsx
@@ -0,0 +1,30 @@
+interface IconModalProps {
+  name: string;
+  hide?: boolean;
+  whiteSpaceWrap?: boolean;
+}
+
+const IconModal = ({
+  name,
+  hide = true,
+  whiteSpaceWrap = false,
+}: IconModalProps): JSX.Element => {
+  return (
+    <div
+      hidden={hide}
+      className={`text-white text-center text-xs bg-home-bg-color rounded-md p-[0.6em] absolute ${
+        whiteSpaceWrap ? "bottom-[-4.9rem]" : "bottom-[-2.4rem]"
+      }`}
+    >
+      <p
+        className={`${
+          whiteSpaceWrap ? "whitespace-pre-wrap" : "whitespace-nowrap"
+        }`}
+      >
+        {name}
+      </p>
+    </div>
+  );
+};
+
+export default IconModal;
