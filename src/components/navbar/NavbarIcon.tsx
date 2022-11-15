93e028c5b6fcb5a76a3a26abf6b44a2f7d5fc750
Dylan Buchi <devybuchi@gmail.com>
2022-11-15 08:49:45 -0300

    Add name prop and add IconModal to NavbarIcon

--- src/components/navbar/NavbarIcon.tsx
+++ src/components/navbar/NavbarIcon.tsx
@@ -1,16 +1,28 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import IconModal from "../IconModal";
 
 interface NavbarIconProps {
   icon: ReactNode;
   classes?: string;
+  name?: string;
 }
-const NavbarIcon = ({ icon, classes = "" }: NavbarIconProps): JSX.Element => {
+const NavbarIcon = ({
+  name = "",
+  icon,
+  classes = "",
+}: NavbarIconProps): JSX.Element | null => {
+  const [hideModal, setHideModal] = useState(true);
   return (
-    <div
-      className={`text-icons-color grid place-items-center w-[2.6rem] h-[2.6rem] sm:w-[3.5rem] sm:h-[3.5rem] hover:rounded-full hover:cursor-pointer hover:bg-icons-hover-color text-[1.5rem] ${classes}`}
-    >
-      {icon}
-    </div>
+    <>
+      <div
+        onMouseOver={() => setHideModal(false)}
+        onMouseLeave={() => setHideModal(true)}
+        className={`text-icons-color grid place-items-center w-[2.6rem] h-[2.6rem] sm:w-[3.5rem] sm:h-[3.5rem] hover:rounded-full hover:cursor-pointer hover:bg-icons-hover-color text-[1.5rem] relative ${classes}`}
+      >
+        {icon}
+        {name.length > 0 && <IconModal name={name} hide={hideModal} />}
+      </div>
+    </>
   );
 };
 
